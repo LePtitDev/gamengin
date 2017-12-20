43987fc98a4e08ff5f62059e1ca821eff1db2b64
LePtitDev <perso@leptitdev.com>
2017-12-20 19:29:33 +0100

    Ajout des dernières pieces

--- assets/scenes/main.lua
+++ assets/scenes/main.lua
@@ -3,10 +3,13 @@
 
 -- Chargement des prefabs
 LoadPrefab("prefab:camera", "assets/prefabs/camera.prefab")
-LoadPrefab("prefab:tower", "assets/prefabs/tower.prefab")
 LoadPrefab("prefab:pawn", "assets/prefabs/pawn.prefab")
+LoadPrefab("prefab:rook", "assets/prefabs/rook.prefab")
+LoadPrefab("prefab:knight", "assets/prefabs/knight.prefab")
 LoadPrefab("prefab:bishop", "assets/prefabs/bishop.prefab")
+LoadPrefab("prefab:queen", "assets/prefabs/queen.prefab")
+LoadPrefab("prefab:king", "assets/prefabs/king.prefab")
 
 -- Contenu de la scene
 GameObject.Instanciate("prefab:camera")
-GameObject.Instanciate("prefab:bishop")
+GameObject.Instanciate("prefab:queen")
